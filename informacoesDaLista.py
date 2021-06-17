4f44e4a8390195fe701a0f5676b35e8662e896c4
Claudenir Ferreira <claudenir_fer@yahoo.com.br>
2021-06-17 12:04:31 -0300

    Ver:01

---
+++ informacoesDaLista.py
@@ -0,0 +1,11 @@
+lista = [5, 7, 2, 9, 4, 1, 3]
+
+print(len(lista))
+print(max(lista))
+print(min(lista))
+print(sum(lista))
+#print(sorted(lista))
+lista.sort()
+print(lista)
+lista.reverse()
+print(lista)
